f0092cb200eb1524c4d4c63c39628a233cf236be
MYusron12 <ryusron19@gmail.com>
2020-04-09 17:52:27 +0700

    initialize

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,11 +1,12 @@
 <?php
-
+//class home turunan dari ci controller
 class Home extends CI_Controller
 {
     public function index($nama = '')
     {
         $data['judul'] = 'Halaman Home';
         $data['nama'] = $nama;
+        //fungsi untuk me load
         $this->load->view('templates/header', $data);
         $this->load->view('home/index', $data);
         $this->load->view('templates/footer');
