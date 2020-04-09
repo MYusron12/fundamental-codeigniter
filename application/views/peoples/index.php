195964701b42238458d63682151d9e183748aa99
MYusron12 <ryusron19@gmail.com>
2020-04-09 19:48:31 +0700

    pagination

---
+++ application/views/peoples/index.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+        
+        <h3 class="mt-3">List of Peoples</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i = 1; foreach ($peoples as $people) : ?>
+                <tr>
+                    <th><?= $i++ ?></th>
+                    <td><?= $people['name']; ?></td>
+                    <td><?= $people['email']; ?></td>
+                    <td>
+                        <a href="" class="badge badge-success">detail</a>
+                        <a href="" class="badge badge-primary">ubah</a>
+                        <a href="" class="badge badge-danger">hapus</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        
+        </div>
+    </div>
+</div>
