6fe6bf505aadb6009712184687e5d933ddc84515
MYusron12 <ryusron19@gmail.com>
2020-04-12 17:14:07 +0700

    update

--- application/controllers/Peoples.php
+++ application/controllers/Peoples.php
@@ -9,8 +9,36 @@ class Peoples extends CI_Controller
 
         $this->load->model('Peoples_model', 'peoples');
 
-        $data['peoples'] = $this->peoples->getAllPeoples();
-        
+        //pagination
+        //load library
+        $this->load->library('pagination');
+
+        //ambil data keyword
+        if ($this->input->post('submit')) {
+            $data['keyword'] = $this->input->post('keyword');
+            $this->session->set_userdata('keyword', $data['keyword']);
+        } else {
+            $data['keyword'] = $this->session->userdata('keyword');
+        }
+
+        //
+        $this->db->like('name', $data['keyword']);
+        $this->db->or_like('email', $data['keyword']);
+        $this->db->from('peoples');
+        $config['total_rows'] = $this->db->count_all_results();
+        $data['total_rows'] = $config['total_rows'];
+        $config['per_page'] = 10;
+
+        //initialize
+        $this->pagination->initialize($config);
+
+
+        $data['start'] = $this->uri->segment(3);
+
+        //isi paramter yaitu limit data dan data yg ditampikan
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
+
+
         $this->load->view('templates/header', $data);
         $this->load->view('peoples/index', $data);
         $this->load->view('templates/footer');
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,33 +1,54 @@
 <div class="container">
+    <h3 class="mt-3">List of Peoples</h3>
+
+    <div class="row">
+        <div class="col-md-7">
+            <form action="<?= base_url('peoples'); ?>" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="search" name="keyword" autocomplete="off" autofocus>
+                    <div class="input-group-append">
+                        <input class="btn btn-primary" type="submit" name="submit">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-10">
-        
-        <h3 class="mt-3">List of Peoples</h3>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1; foreach ($peoples as $people) : ?>
-                <tr>
-                    <th><?= $i++ ?></th>
-                    <td><?= $people['name']; ?></td>
-                    <td><?= $people['email']; ?></td>
-                    <td>
-                        <a href="" class="badge badge-success">detail</a>
-                        <a href="" class="badge badge-primary">ubah</a>
-                        <a href="" class="badge badge-danger">hapus</a>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        
+            <small>hasil <?= $total_rows; ?> data</small>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if (empty($peoples)) : ?>
+                        <tr>
+                            <td colspan="4">
+                                <div class="alert alert-info" role="alert">Data tdk ada
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endif; ?>
+                    <?php foreach ($peoples as $people) : ?>
+                        <tr>
+                            <th><?= ++$start; ?></th>
+                            <td><?= $people['name']; ?></td>
+                            <td><?= $people['email']; ?></td>
+                            <td>
+                                <a href="" class="badge badge-success">detail</a>
+                                <a href="" class="badge badge-primary">ubah</a>
+                                <a href="" class="badge badge-danger">hapus</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+            <?= $this->pagination->create_links(); ?>
         </div>
     </div>
 </div>
